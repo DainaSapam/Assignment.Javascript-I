b1514d39c22ca0cd769a2b02989f2d64711b7c52
DainaSapam <dainasapam8@gmail.com>
2016-11-30 12:58:16 +0530

    Add files via upload

---
+++ Assignment 5.1/js/app.js
@@ -0,0 +1,10 @@
+var name = "Daina";
+var age = 16;
+var dateOfBirth = "15-9-2000";
+var placeOfBirth = "Place";
+
+//console.log(name + age + dateOfBirth + placeOfBirth);
+console.log("Name: "+name);
+console.log("Age: "+age);
+console.log("Date of Birth: "+dateOfBirth);
+console.log("Place of Birth: "+placeOfBirth);
